db0783aeb326a2deee2b933c93868bd3765c8815
Jacob Halsey <jacob@jhalsey.com>
2022-07-27 20:48:50 +0100

    Attempt to fix docs.rs

--- cspice-sys/CHANGES.md
+++ cspice-sys/CHANGES.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.1
+
+Fix docs.rs documentation
+
 ## 1.0.0
 
 Initial release. This is a complete replacement to the 0.x versions on crates.io.
--- cspice-sys/build.rs
+++ cspice-sys/build.rs
@@ -1,16 +1,22 @@
+extern crate core;
+
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 const CSPICE_DIR: &str = "CSPICE_DIR";
 const CSPICE_CLANG_TARGET: &str = "CSPICE_CLANG_TARGET";
 const CSPICE_CLANG_ROOT: &str = "CSPICE_CLANG_ROOT";
 
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    if std::env::var("DOCS_RS").is_ok() {
+        docs_rs(&out_path);
+    }
+
     println!("cargo:rerun-if-env-changed={}", CSPICE_DIR);
     println!("cargo:rerun-if-env-changed={}", CSPICE_CLANG_TARGET);
     println!("cargo:rerun-if-env-changed={}", CSPICE_CLANG_ROOT);
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let cspice_dir = match env::var(CSPICE_DIR) {
         Ok(cspice_dir) => PathBuf::from(cspice_dir),
@@ -54,4 +60,26 @@ fn main() {
         cspice_dir.join("lib").display()
     );
     println!("cargo:rustc-link-lib=static=cspice");
+}
+
+// For docs.rs only we will download and extract the CSPICE library automatically
+// It is not a good idea to do this in general though, it should be specific to the user / platform
+// https://kornel.ski/rust-sys-crate
+fn docs_rs(out_dir: &Path) {
+    let outfile = out_dir.join("cspice.tar.Z");
+    let curl_status = Command::new("curl")
+        .arg("https://naif.jpl.nasa.gov/pub/naif/toolkit//C/PC_Linux_GCC_64bit/packages/cspice.tar.Z")
+        .arg("-o")
+        .arg(outfile.display().to_string())
+        .status()
+        .expect("Unable to call curl");
+    assert!(curl_status.success());
+    let tar_status = Command::new("tar")
+        .arg("-zxf")
+        .arg("cspice.tar.Z")
+        .current_dir(out_dir)
+        .status()
+        .expect("Unable to call tar");
+    assert!(tar_status.success());
+    env::set_var("CSPICE_DIR", out_dir.join("cspice").as_os_str());
 }
